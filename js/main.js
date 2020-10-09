3df00ff4cae6f0bbd7d99968638c27e7b7a41306
lenokand <72378305+lenokand@users.noreply.github.com>
2020-10-09 22:47:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const cartButton= document.querySelector('#card-button');
+const modal = document.querySelector ('.modal');
+const close = document.querySelector ('.close');
+
+cartButton.addEventListener('click', toggleModal
+);
+close.addEventListener('click', toggleModal
+);
+function toggleModal(){
+    modal.classList.toggle("is-open");
+}
+
+
+new WOW().init();
